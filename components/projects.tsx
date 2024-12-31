b881f7de0d7ad9811ed0a0fb62de2f23500a638c
Riddhimaan-Senapati <rsenapati@umass.edu>
2024-12-31 17:27:08 +0530

    added project links to my portfolio website

--- components/projects.tsx
+++ components/projects.tsx
@@ -1,6 +1,7 @@
 import Image from 'next/image'
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { Badge } from '@/components/ui/badge'
+import Link from 'next/link'
 
 const projects = [
   {
@@ -8,24 +9,30 @@ const projects = [
     description: 'An AI-powered schedule builder for farmers',
     image: '/plowtion.png',
     tags: ['Next.Js', 'Docker','Tensorflow'],
+    link: 'https://github.com/Aadityaa2004/Plowtion',
   },
+
   {
     title: 'Codestrike',
     description: 'A CS-GO inspired website where you can PvP leetcode problems',
     image: '/codestrike.png',
     tags: ['React.Js', 'Docker','Express.Js','Node.Js'],
+    link: 'https://github.com/craigbsch/CodeStrike',
   },
+
   {
     title: 'Course Advisor',
     description: 'An AI-powered app to analyze course descriptions, provide recommendations and see professor rankings',
     image: '/placeholder.svg?height=200&width=300',
     tags: ['Streamlit', 'OpenAI','RESTful API'],
+    link: 'https://github.com/Riddhimaan-Senapati/CourseAdvisor',
   },
   {
     title: 'Airport Coordinator',
     description: 'A website to help international students coordinate their trips for ridesharing',
     image: '/airport_coordinator.jpg',
     tags: ['React.Js', 'MongoDB','Node.Js'],
+    link: 'https://github.com/Riddhimaan-Senapati/Airport-Coordinator',
   },
 ]
 
@@ -37,16 +44,22 @@ const Projects = () => {
         {projects.map((project) => (
           <Card key={project.title}>
             <CardHeader>
-              <CardTitle>{project.title}</CardTitle>
+              <CardTitle>
+                <Link href={project.link} target="_blank" rel="noopener noreferrer" className="hover:underline">
+                  {project.title}
+                </Link>
+              </CardTitle>
             </CardHeader>
             <CardContent>
-              <Image
-                src={project.image}
-                alt={project.title}
-                width={300}
-                height={200}
-                className="mb-4 rounded-lg object-cover"
-              />
+              <Link href={project.link} target="_blank" rel="noopener noreferrer">
+                <Image
+                  src={project.image}
+                  width={300}
+                  height={200}
+                  className="mb-4 rounded-lg object-cover"
+                  alt=""
+                />
+              </Link>
               <CardDescription>{project.description}</CardDescription>
             </CardContent>
             <CardFooter>
@@ -64,4 +77,3 @@ const Projects = () => {
 }
 
 export default Projects
-
