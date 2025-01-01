42185776fe7d75598171fd6c4b953e455b1974f2
Riddhimaan-Senapati <rsenapati@umass.edu>
2025-01-01 14:44:16 +0530

    added a yellow colored resume button that points to my resume on google drive

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
+
 const Hero = () => {
   return (
     <section className="py-24 flex flex-col md:flex-row items-center justify-between">
@@ -25,6 +26,12 @@ const Hero = () => {
         <Button className="mt-8" size="lg" asChild>
           <a href="#contact">Get in Touch</a>
         </Button>
+        
+        <Button size="lg" variant="outline" className="bg-yellow-400 hover:bg-yellow-500 text-black border-yellow-500" asChild>
+            <a href="https://drive.google.com/file/d/12t5e_3T9QRfO4jA4o0rEvoXvEpeAFCEL/view?usp=sharing" target="_blank" rel="noopener noreferrer">
+              View Resume
+            </a>
+          </Button>
       </div>
     </section>
   )
